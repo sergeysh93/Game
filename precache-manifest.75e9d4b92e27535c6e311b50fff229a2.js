997c7b2f4b4d6c78a303485d1d4fa66e20c1f99c
Sergey <sergey.shishonkov@gmail.com>
2020-01-14 18:59:08 +0300

    Updates

---
+++ precache-manifest.75e9d4b92e27535c6e311b50fff229a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8d34d3e1f0b9cb1542be55704cb6ee8",
+    "url": "/game/index.html"
+  },
+  {
+    "revision": "ba9887095a3058e7f6c2",
+    "url": "/game/static/css/main.cd508da2.chunk.css"
+  },
+  {
+    "revision": "544c876ceda847331b13",
+    "url": "/game/static/js/2.b8a7688f.chunk.js"
+  },
+  {
+    "revision": "ba9887095a3058e7f6c2",
+    "url": "/game/static/js/main.5cb35fd2.chunk.js"
+  },
+  {
+    "revision": "bab8f84a0f4d6b7ca58b",
+    "url": "/game/static/js/runtime~main.2d0fbc73.js"
+  },
+  {
+    "revision": "8034d35f093c3e4afa1ddcb70c40a3cc",
+    "url": "/game/static/media/antique-backdrop-background-129731.8034d35f.jpg"
+  }
+]);
